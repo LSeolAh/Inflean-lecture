62f9799c71796e2a675d760f0440847a079c20e3
LSeolAh <sualah0417@gmail.com>
2023-01-04 23:02:31 +0900

    TodoHeader 컴포넌트 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -23,4 +23,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  text-align: center;
+  background-color: silver;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
