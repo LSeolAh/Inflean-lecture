630108fdf9d516e6111c5cc9dcf8111cbb98ac48
LSeolAh <sualah0417@gmail.com>
2023-01-18 14:50:35 +0900

    [리팩토링] 할 일 목록 표시 기능

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div>
     <ul>
       <li
-        v-for="(todoItem, index) in todoItems"
+        v-for="(todoItem, index) in propsdata"
         v-bind:key="todoItem.item"
         class="shadow"
       >
@@ -24,11 +24,7 @@
 
 <script>
 export default {
-  data: function () {
-    return {
-      todoItems: [],
-    };
-  },
+  props: ["propsdata"],
   methods: {
     removeTodo: function (todoItem, index) {
       console.log(todoItem, index);
@@ -43,19 +39,6 @@ export default {
       // localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
       //index 사용안하면 오류 생겨서 해둔 오류 방지용
     },
-  },
-  created: function () {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server")
-          this.todoItems.push(
-            JSON.parse(localStorage.getItem(localStorage.key(i)))
-          );
-
-        // this.todoItems.push(localStorage.key(i));
-        // console.log(localStorage.key(i));
-      }
-    }
   },
 };
 </script>
