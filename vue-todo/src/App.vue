78e0c841b7e24bacdb7810817ca067fc7fd8eba5
LSeolAh <sualah0417@gmail.com>
2023-01-30 23:23:35 +0900

    [리팩토링] mutations 퀴즈 풀이 및 할 일 지우기, 할 일 추가, 완료, 모두 삭제 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -2,12 +2,8 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
-    <TodoList
-      v-bind:propsdata="todoItems"
-      v-on:removeItem="removeOneItem"
-      v-on:toggleItem="toggleOneItem"
-    ></TodoList>
-    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -18,35 +14,6 @@ import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
 
 export default {
-  data() {
-    return {
-      todoItems: [],
-    };
-  },
-  methods: {
-    removeOneItem(todoItem, index) {
-      localStorage.removeItem(todoItem.item);
-      this.todoItems.splice(index, 1);
-    },
-    toggleOneItem(todoItem, index) {
-      this.todoItems[index].completed = !this.todoItems[index].completed;
-      //위에 있는 코드와 같은 동작을 하지만
-      // 컴포넌트의 경계를 명확하게 하기 위해 위에 코드를 사용
-      // todoItem.completed = !todoItem.completed;
-
-      //로컬스토리지에 데이터를 갱신하는 것
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-      // localStorage.removeItem(todoItem.item);
-      // localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-      //index 사용안하면 오류 생겨서 해둔 오류 방지용
-      console.log(index);
-    },
-    clearAllItems() {
-      localStorage.clear();
-      this.todoItems = [];
-    },
-  },
-
   components: {
     TodoHeader,
     TodoInput,
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -26,9 +26,25 @@ export const store = new Vuex.Store({
   mutations: {
     addOneItem(state, todoItem) {
       const obj = { completed: false, item: todoItem };
-      //local storage에 저장
       localStorage.setItem(todoItem, JSON.stringify(obj));
       state.todoItems.push(obj);
+    },
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item);
+      state.todoItems.splice(payload.index, 1);
+    },
+    toggleOneItem(state, payload) {
+      state.todoItems[payload.index].completed =
+        !state.todoItems[payload.index].completed;
+      //   localStorage.removeItem(payload.todoItem.item);
+      localStorage.setItem(
+        payload.todoItem.item,
+        JSON.stringify(payload.todoItem)
+      );
+    },
+    clearAllItems(state) {
+      localStorage.clear();
+      state.todoItems = [];
     },
   },
 });
