c88e0354022c470e8b80600f8560bf3dbdbe6d67
LSeolAh <sualah0417@gmail.com>
2023-01-31 20:10:05 +0900

    스토어 속성 모듈화 방법

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as getters from "./getters";
+import * as mutations from "./mutations";
 
 Vue.use(Vuex);
 
@@ -10,9 +12,6 @@ const storage = {
       for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== "loglevel:webpack-dev-server")
           arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-
-        // this.todoItems.push(localStorage.key(i));
-        // console.log(localStorage.key(i));
       }
     }
     return arr;
@@ -23,33 +22,6 @@ export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch(),
   },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    },
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed =
-        !state.todoItems[payload.index].completed;
-      //   localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    },
-  },
+  getters: getters,
+  mutations: mutations,
 });
