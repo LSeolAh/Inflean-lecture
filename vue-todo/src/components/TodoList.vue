4ed56836ff54054862b9685d5ce8723c08ebd673
LSeolAh <sualah0417@gmail.com>
2023-01-31 19:26:28 +0900

    [리팩토링 & 퀴즈] mapMutations 적용 및 퀴즈

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -14,7 +14,7 @@
         <span v-bind:class="{ textCompleted: todoItem.completed }">{{
           todoItem.item
         }}</span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span class="removeBtn" v-on:click="removeTodo({ todoItem, index })">
           <i class="fa-solid fa-trash-can"></i>
         </span>
       </li>
@@ -23,9 +23,13 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
+
 export default {
   methods: {
+    ...mapMutations({
+      removeTodo: "removeOneItem",
+    }),
     removeTodo(todoItem, index) {
       this.$store.commit("removeOneItem", { todoItem, index });
     },
