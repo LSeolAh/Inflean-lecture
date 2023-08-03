05d4357bd357332d061ed6a140cf72af54be0458
SNXWXH <snxwxh@gmail.com>
2023-08-04 00:55:37 +0900

    array, object state 변경하는 법

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -7,7 +7,7 @@ function App() {
   // let [글제목1, a] = useState("남자 코트 추천");
   // let [글제목2, b] = useState("강남 맛집 추천");
   // let [글제목3, c] = useState("파이썬 독학");
-  let [글제목, b] = useState([
+  let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
     "강남 맛집 추천",
     "파이썬 독학",
@@ -23,6 +23,23 @@ function App() {
       <div className="black-nav">
         <h4>설라 블로그</h4>
       </div>
+      <button
+        onClick={() => {
+          let sort = [...글제목].sort();
+          글제목변경(sort);
+        }}
+      >
+        가나다순 정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자 코트 추천";
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
       <div className="list">
         <h4>
           {글제목[0]}
