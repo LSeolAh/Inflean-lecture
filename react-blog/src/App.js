782910bf69b4d7aadc771e048b4e0c145120198e
SNXWXH <snxwxh@gmail.com>
2023-08-03 17:11:20 +0900

    중요한 데이터는 변수말고 state에 담습니다

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,16 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [글제목1, a] = useState("남자 코트 추천");
+  let [글제목2, b] = useState("강남 맛집 추천");
+  let [글제목3, c] = useState("파이썬 독학");
+  //let [글제목, b] = useState(["남자 코트 추천", "강남 맛집 추천", "파이썬 독학");
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>설라 블로그</h4>
       </div>
-      <h4>{post}</h4>
-      <div></div>
+      <div className="list">
+        <h4>{글제목1}</h4>
+        <p>8월 3일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>8월 3일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목3}</h4>
+        <p>8월 3일 발행</p>
+      </div>
     </div>
   );
 }
