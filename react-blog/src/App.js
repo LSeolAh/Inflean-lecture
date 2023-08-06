bad7ff1173878cc08a3207a652cf367e6256cf11
SNXWXH <snxwxh@gmail.com>
2023-08-06 23:41:15 +0900

    자식이 부모의 state 가져다쓰고 싶을 때는 props

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -20,60 +20,11 @@ function App() {
       <div className="black-nav">
         <h4>설라 블로그</h4>
       </div>
-      <button
-        onClick={() => {
-          let sort = [...글제목].sort();
-          글제목변경(sort);
-        }}
-      >
-        정렬버튼
-      </button>
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy[0] = "여자 코트 추천";
-          글제목변경(copy);
-        }}
-      >
-        글수정
-      </button>
-      {/* <div className="list">
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉++);
-            }}
-          >
-            👍
-          </span>
-          {따봉}
-        </h4>
-        <p>8월 3일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>8월 3일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            modal === false ? setModal(true) : setModal(false);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-
-        <p>8월 3일 발행</p>
-      </div> */}
-      {modal === true ? <Modal /> : null}
-
-      <Hi></Hi>
-      {글제목.map((v, i) => {
+      {글제목.map(function (v, i) {
         return (
-          <div className="list">
-            <h4>
-              {i + 1}. {v}
+          <div className="list" key={i}>
+            <h4 onClick={() => setModal(true)}>
+              {글제목[i]}
               <span
                 onClick={() => {
                   let copy = [...따봉];
@@ -85,20 +36,32 @@ function App() {
               </span>
               {따봉[i]}
             </h4>
-            <p>8월 3일 발행</p>
+            <p>8월 6일 발행</p>
           </div>
         );
       })}
+      {modal === true ? (
+        <Modal color={"yellow"} 글제목변경={글제목변경} 글제목={글제목} />
+      ) : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{ background: props.color }}>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.글제목];
+          copy[0] = "여자코트추천";
+          props.글제목변경(copy);
+        }}
+      >
+        변경
+      </button>
     </div>
   );
 }
