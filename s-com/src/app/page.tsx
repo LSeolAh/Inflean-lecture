86cb75ecb08c77be1f5f2bebb4d3ebd524b07bb2
SNXWXH <snxwxh@gmail.com>
2024-05-20 00:09:02 +0900

    css module을 선택한 이유

--- s-com/src/app/page.tsx
+++ s-com/src/app/page.tsx
@@ -5,21 +5,22 @@ import zLogo from '../../public/zlogo.png';
 
 export default function Home() {
   return (
-    <div>
-      <div className='styles.left'>
+    <>
+      <div className={styles.left}>
         <Image src={zLogo} alt='logo' />
       </div>
-      <div className='styles.right'>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요</h2>
         {/* <a>는 페이지가 새로고침되며 넘어감 */}
         <Link href='/i/flow/signup' className={styles.signup}>
           계정 만들기
         </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
         <Link href='/login' className={styles.login}>
           로그인
         </Link>
       </div>
-    </div>
+    </>
   );
 }
