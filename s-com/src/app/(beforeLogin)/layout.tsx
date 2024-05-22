4de939c88b06739b0398b52c23ff45b795026838
SNXWXH <snxwxh@gmail.com>
2024-05-22 13:57:02 +0900

    레이아웃 클론하기

--- s-com/src/app/(beforeLogin)/layout.tsx
+++ s-com/src/app/(beforeLogin)/layout.tsx
@@ -4,10 +4,5 @@ import styles from '@/app/(beforeLogin)/_component/main.module.css';
 type Props = { children: ReactNode; modal: ReactNode };
 
 export default function Layout({ children, modal }: Props) {
-  return (
-    <div className={styles.container}>
-      비포 로그인 레이아웃
-      {children}
-    </div>
-  );
+  return <div className={styles.container}>{children}</div>;
 }
